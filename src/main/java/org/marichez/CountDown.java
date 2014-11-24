083170b492746be212d1c66f4045a3458064a817
nicolas marichez <marichez.nicolas@live.fr>
2014-11-24 12:15:47 +0100

    change the date-format and the date

--- src/main/java/org/marichez/CountDown.java
+++ src/main/java/org/marichez/CountDown.java
@@ -38,8 +38,8 @@ public void doGet( HttpServletRequest request, HttpServletResponse response )
 
     }*/
     private String diff(){
-        String theDate = "24/11/2014 12:10:00";
-        String pattern = "dd/MM/yyyy HH:mm:ss";
+        String theDate = "11-24-2014 12:10:00";
+        String pattern = "MM-dd-yyyy HH:mm:ss";
         Date d2 = null;
         try {
             d2 = new SimpleDateFormat(pattern).parse(theDate);
